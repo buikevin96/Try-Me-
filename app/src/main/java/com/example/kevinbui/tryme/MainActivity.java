e1c12566613932c066a2c77c0e72c7ad2ac8b1be
buikevin96 <buikevin96@gmail.com>
2019-02-24 18:17:25 -0500

    Added Colors and randomized the change in background color when button is clicked

--- app/src/main/java/com/example/kevinbui/tryme/MainActivity.java
+++ app/src/main/java/com/example/kevinbui/tryme/MainActivity.java
@@ -3,23 +3,44 @@ package com.example.kevinbui.tryme;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private View windowView; // Background behind the button
     private Button tryMeButton;
+    private int[] colors; // Array of colors
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        windowView = findViewById(R.id.windowViewId);
+        colors = new int[]{Color.CYAN, Color.GREEN, Color.RED, Color.BLUE, Color.BLACK, Color.DKGRAY,
+        Color.LTGRAY, Color.MAGENTA, Color.YELLOW, Color.WHITE};
 
-        windowView.setBackgroundColor(Color.RED); // Sets the background color to red
+        windowView = findViewById(R.id.windowViewId); // Connect window to ID
+        windowView.setBackgroundColor(colors[2]);
+
         tryMeButton = (Button) findViewById(R.id.tryMeButton);
+        tryMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logic that will be called when button is tapped
+                int colorArrayLength = colors.length;
+
+                Random random = new Random();
+                int randomNum = random.nextInt(colorArrayLength);
+
+                windowView.setBackgroundColor(colors[randomNum]); // Set background to color array
+
+                Log.d("Random", String.valueOf(randomNum));
+            }
+        });
 
 
     }
